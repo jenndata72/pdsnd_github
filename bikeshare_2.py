53f9a91545fdd6d03f03779d914e33a4d4d8aae7
Jennifer Close <jennclose72@gmail.com>
2023-07-02 15:51:39 -0400

    refactor: Update user restart question

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -258,7 +258,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         display_raw_data(df)
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart to select different options? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
